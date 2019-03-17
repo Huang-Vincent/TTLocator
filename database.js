ab9439431f4fd5ec39049246afe680957619310d
Vincent Huang <vinhuang848@gmail.com>
2019-03-17 03:02:21 -0400

    Asd

--- database.js
+++ database.js
@@ -34,10 +34,8 @@ function initMap() {
     },
   });
 
-
   google.maps.event.addListener(map, 'click', function(event) {
-   placeMarker(event.latLng);
-
+    placeMarker(event.latLng);
   });
 }
 
@@ -52,54 +50,8 @@ function placeMarker(location) {
         url: "blue-dot.png"
       }
     });
-    var yesno;
-    setTimeout(function() {
-      yesno = confirm("Do you want to start a #trashtag here?");
-    }, 300);
-    if (yesno) {
-      var contentString = '<div id="content">'+
-             '<div id="siteNotice">'+
-             '</div>'+
-             '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-             '<div id="bodyContent">'+
-             '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-             'sandstone rock formation in the southern part of the '+
-             'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-             'south west of the nearest large town, Alice Springs; 450&#160;km '+
-             '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-             'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-             'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-             'Aboriginal people of the area. It has many springs, waterholes, '+
-             'rock caves and ancient paintings. Uluru is listed as a World '+
-             'Heritage Site.</p>'+
-             '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-             'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-             '(last visited June 22, 2009).</p>'+
-             '</div>'+
-             '</div>';
-
-         var infowindow = new google.maps.InfoWindow({
-           content: contentString
-         });
-
-         infowindow.open(map, marker1);
-    } else {
-    }
   }
 }
-
-function codeAddress(i_title, address) {
-    geocoder.geocode( { 'address': address}, function(results, status) {
-      console.log(status);
-      if (status == 'OK') {
-        var marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location,
-            title: i_title
-        });
-      }
-    });
-  }
 
 function addMarkers() {
   peopleRef.once('value')
@@ -118,9 +70,21 @@ function addMarkers() {
           title: results[0]
         });
         marker.setMap(map);
-
     })
   })
+}
+
+writeUserData(19.2059, -1.508, 1849285432, "@yestindee");
+
+function writeUserData(lat, long, number, twitter) {
+  firebase.database().ref('Clients/'+twitter+'/Coordinates').set({
+    Lat: lat,
+    Long: long,
+  });
+
+  firebase.database().ref('Clients/'+twitter).update({
+    Number: number,
+  });
 }
 
 addMarkers();
