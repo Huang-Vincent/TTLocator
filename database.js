d68a8ce7b9a6f0774f9d42fd85f549b5a9e47b5a
Raunak Shrestha <21987770+Raunak-S@users.noreply.github.com>
2019-03-17 00:55:03 -0400

    recursion

--- database.js
+++ database.js
@@ -19,19 +19,47 @@ function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 39.828, lng: -98.579},
     zoom: 3.75,
-    minZoom: 2.5
+    minZoom: 2.5,
+    disableDoubleClickZoom: true
   });
 
-  if(marker1) {
-    marker1.addListener('click', function() {
-      infowindow.open(map, this);
-    });
-  }
 
   google.maps.event.addListener(map, 'click', function(event) {
    placeMarker(event.latLng);
-  });
+   var yesno;
+   setTimeout(function() {
+     yesno = confirm("Do you want to start a #trashtag here?");
+   }, 300);
+   if (yesno) {
+     var contentString = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+            '<div id="bodyContent">'+
+            '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+            'sandstone rock formation in the southern part of the '+
+            'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+            'south west of the nearest large town, Alice Springs; 450&#160;km '+
+            '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+            'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+            'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+            'Aboriginal people of the area. It has many springs, waterholes, '+
+            'rock caves and ancient paintings. Uluru is listed as a World '+
+            'Heritage Site.</p>'+
+            '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+            'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+            '(last visited June 22, 2009).</p>'+
+            '</div>'+
+            '</div>';
 
+        var infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+
+        infowindow.open(map, popup);
+
+   }
+  });
 }
 
 function placeMarker(location) {
@@ -41,7 +69,9 @@ function placeMarker(location) {
     marker1 = new google.maps.Marker({
       position: location,
       map: map,
-      animation: google.maps.Animation.DROP,
+      icon: {
+        url: "blue-dot.png"
+      }
     });
   }
 }
@@ -53,10 +83,7 @@ function codeAddress(i_title, address) {
         var marker = new google.maps.Marker({
             map: map,
             position: results[0].geometry.location,
-            title: i_title,
-            icon: {
-              url: "blue-dot.png"
-            }
+            title: i_title
         });
       }
     });
@@ -74,18 +101,12 @@ function addMarkers() {
         results.push(childData);
         numElements++;
       })
-      if (numElements == 2) {
-        codeAddress(results[0], results[1]);
-      } else if (numElements == 3) {
         var marker = new google.maps.Marker({
           position: {lat: results[1], lng: results[2]},
-          title: results[0],
-          icon: {
-            url: "blue-dot.png"
-          }
+          title: results[0]
         });
         marker.setMap(map);
-      }
+
     })
   })
 }
