f06187465722c5b3d773de6ed3f6e061ebfc34b1
Vincent Huang <vinhuang848@gmail.com>
2019-03-17 06:02:51 -0400

    ysh

--- database.js
+++ database.js
@@ -42,8 +42,7 @@ function initMap() {
    setTimeout(function() {
      var yesno = confirmPlacement();
      if (yesno) {
-       $( "div.popup" ).show();
-       $( "#map" ).hide();
+       openForm();
        personLoc = event.latLng;
      }
    }, 200);
@@ -99,14 +98,16 @@ function writeUserData(lat, long, number, twitter) {
     Number: number
   });
 }
+var modal = document.getElementById('myModal');
 
 function openForm() {
-  document.getElementById("myForm").style.display = "block";
+  // document.getElementById("myForm").style.display = "block";
+  // Get the modal
+  modal.style.display = "block";
 }
 
 function closeForm() {
-  $( "div.popup" ).hide();
-  $( "#map" ).show();
+  modal.style.display = "none";
   document.getElementById("myForm").style.display = "none";
 }
 
@@ -114,4 +115,19 @@ function submitForm() {
   writeUserData(personLoc.lat(), personLoc.lng(), $("#number").val(), $("#twitter").val());
   $( "div.popup" ).hide();
   $( "#map" ).show();
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
